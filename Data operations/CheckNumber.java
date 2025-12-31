da74d4538120f027d3d53f532cb4ebb3638aebd2
suswanthreddy18 <suswanthreddy18@gmail.com>
2025-12-31 17:03:04 +0530

    Add files via upload

---
+++ Data operations/CheckNumber.java
@@ -0,0 +1,11 @@
+class CheckNumber {
+    public static void main(String[] args) {
+        int num = 50;
+
+        if(num > 0 && num < 100) {
+            System.out.println("Number is valid");
+        } else {
+            System.out.println("Number is not valid");
+        }
+    }
+}
---
+++ Data operations/UnaryExample.java
@@ -0,0 +1,11 @@
+class UnaryExample {
+    public static void main(String[] args) {
+        int x = 5;
+
+        x++;   // increment
+        System.out.println("After increment: " + x);
+
+        x--;   // decrement
+        System.out.println("After decrement: " + x);
+    }
+}
